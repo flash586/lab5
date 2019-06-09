8e97ca72bcb40724d44e45916273179757e246cc
flash586 <ironpatriot864@gmail.com>
2019-06-09 04:59:43 +0300

    Add files via upload

---
+++ Matrix.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    class Matrix
+    {
+        public int n = 4;
+        public double K1 = 0.0012, K2 = 0.0012;
+        public double[,] B2 = { { 4, 2, 5, 7 }, { 7, 5, 10, 9 }, { 8, 7, 4, 3 }, { 3, 7, 4, 8 } };
+        public double[,] A2 = { { 2, 5, 1, 4 }, { 5, 6, 4, 5 }, { 7, 9, 4, 7 }, { 8, 3, 10, 8 } };
+        public double[,] A1 = { { 7, 3, 5, 4 }, { 7, 2, 7, 4 }, { 1, 5, 6, 10 }, { 7, 4, 8, 11 } };
+        public double[,] A = { { 1, 2, 1, 9 }, { 1, 8, 5, 7 }, { 2, 7, 9, 3 }, { 2, 5, 5, 7 } };
+        public double[,] b1 = { { 7, 0, 0, 0 }, { 5, 0, 0, 0 }, { 2, 0, 0, 0 }, { 10, 0, 0, 0 } };
+        public double[,] c1 = { { 2, 0, 0, 0 }, { 4, 0, 0, 0 }, { 7, 0, 0, 0 }, { 7, 0, 0, 0 } };
+        public double[,] C2;
+        public double[,] Y3;
+        public double[,] b;
+        public double[,] y1;
+        public double[,] y2;
+        public double[,] first;
+        public double[,] second;
+        public double[,] third;
+        public double[,] fourth;
+        public double[,] fifth;
+        public double[,] X;
+        public Matrix()
+        {
+            this.C2 = new double[n, n];
+            this.Y3 = new double[n, n];
+            this.b = new double[n, n];
+            this.y1 = new double[n, n];
+            this.y2 = new double[n, n];
+            this.first = new double[n, n];
+            this.second = new double[n, n];
+            this.third = new double[n, n];
+            this.fourth = new double[n, n];
+            this.fifth = new double[n, n];
+            this.X = new double[n, n];
+        }
+
+        public void Y_3()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    C2[i, j] = 1 / (Math.Pow(i+1,2) + j+1);
+                    Y3[i, j] = A2[i, j] * (B2[i, j] +10* C2[i, j]);
+                }
+            }
+        }
+        public void b_()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == 0)
+                        b[i, j] = 5 * (Math.Pow(i+1 , 3));
+                    else
+                        b[i, j] = 0;
+                }
+            }
+        }
+        public void y1_()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                    y1[i, 0] = A[i, 0] * b[i, 0];
+            }
+        }
+        public void y2_()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                    y2[i, 0] = A1[i, 0] * (5*b1[i, 0] - c1[i, 0]);
+            }
+        }
+        public void first_()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                first[i, 0] = Y3[i, 0] * y1[i, 0]*y1[0,i];
+            }
+        }
+        public void second_()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    second[i, j] = (first[i,j]+y2[0,i])*K1;
+                }
+            }
+        }
+        public void third_()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    third[i, j] = K2 * (y1[i, j] * y2[j, i] * y2[i, j] * Math.Pow(Y3[i, j],2));
+                }
+            }
+        }
+        public void fourth_()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    fourth[i, j] = Y3[i, j] * y2[i, j] + y1[i, j];
+                }
+            }
+        }
+        public void fifth_()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    fifth[i, j] = fourth[i,j]+third[i,j];
+                }
+            }
+        }
+        public void x()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    X[i, j] = second[i, j] *fifth[i, j];
+                }
+            }
+        }
+        public void Output()
+        {
+            Console.WriteLine("Y3: ");
+            Print(Y3);
+            Console.WriteLine("b: ");
+            Print(b);
+            Console.WriteLine("y1: ");
+            Print(y1);
+            Console.WriteLine("y2: ");
+            Print(y2);
+            Console.WriteLine("first: ");
+            Print(first);
+            Console.WriteLine("second: ");
+            Print(second);
+            Console.WriteLine("third: ");
+            Print(third);
+            Console.WriteLine("fourth: ");
+            Print(fourth);
+            Console.WriteLine("fifth: ");
+            Print(fifth);
+            Console.WriteLine("X: ");
+            Print(X);
+            void Print(double[,] arr)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write(arr[i, j] + " | ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace lab5
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix();
+            Task Y3 = new Task((matrix.Y_3));
+            Y3.Start();
+            Y3.Wait();
+            Task b = new Task((matrix.b_));
+            b.RunSynchronously();
+            b.Wait();
+            Task y1 = new Task((matrix.y1_));
+            y1.RunSynchronously();
+            y1.Wait();
+            Task y2 = new Task((matrix.y2_));
+            y2.RunSynchronously();
+            y2.Wait();
+
+            Task first = new Task(matrix.first_);
+            first.Start();
+            Task second = new Task(matrix.second_);
+            second.Start();
+            Task third = new Task(matrix.third_);
+            third.Start();
+            Task fourth = new Task(matrix.fourth_);
+            fourth.Start();
+            Task fifth = new Task(matrix.fifth_);
+            fifth.Start();
+            Task x = new Task(matrix.x);
+            x.Start();
+            matrix.Output();
+            Console.ReadKey();
+        }
+    }
+}
